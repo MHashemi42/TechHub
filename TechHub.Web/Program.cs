98487be9eef304d9519d3f5f24d9ffb1e3138f0f
Mohammad Hashemi <mohammadhashemi42@gmail.com>
2023-03-12 14:33:53 +0330

    Add Serilog

--- TechHub.Web/Program.cs
+++ TechHub.Web/Program.cs
@@ -1,5 +1,13 @@
+using Serilog;
+
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseSerilog((context, configuration) => 
+    configuration.ReadFrom.Configuration(context.Configuration));
+
 WebApplication app = builder.Build();
+
+app.UseSerilogRequestLogging();
 
 app.MapGet("/", () => "Hello World!");
 
