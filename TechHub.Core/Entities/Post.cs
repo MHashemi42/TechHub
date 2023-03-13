15a7dc86d13d8822a913885ab4a30ed622c0ad88
Mohammad Hashemi <mohammadhashemi42@gmail.com>
2023-03-13 20:06:40 +0330

    Add Entities

---
+++ TechHub.Core/Entities/Post.cs
@@ -0,0 +1,13 @@
+﻿namespace TechHub.Core.Entities;
+
+public class Post : EntityBase
+{
+    public string Title { get; set; } = default!;
+    public string Slug { get; set; } = default!;
+    public string Content { get; set; } = default!;
+    public string ThumbnailPath { get; set; } = default!;
+    public DateTime DatePublished { get; set; }
+    public string AuthorId { get; set; } = default!;
+
+    public ICollection<Tag> Tags { get; set; } = new List<Tag>();
+}
---
+++ TechHub.Core/Entities/Tag.cs
@@ -0,0 +1,9 @@
+﻿namespace TechHub.Core.Entities;
+
+public class Tag : EntityBase
+{
+    public string Name { get; set; } = default!;
+    public string Slug { get; set; } = default!;
+
+    public ICollection<Post> Posts { get; set; } = new List<Post>();
+}
