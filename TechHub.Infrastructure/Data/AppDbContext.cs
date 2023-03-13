1ecc4a6effb4c2340f1e009d2c5cc6101b27fbf1
Mohammad Hashemi <mohammadhashemi42@gmail.com>
2023-03-13 20:37:24 +0330

    Add DbContext and Identity

---
+++ TechHub.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using TechHub.Core.Entities;
+
+namespace TechHub.Infrastructure.Data;
+
+internal class AppDbContext : IdentityDbContext<ApplicationUser>
+{
+	public AppDbContext(DbContextOptions<AppDbContext> options)
+		: base(options)
+	{
+	}
+
+	public DbSet<Post> Posts => Set<Post>();
+	public DbSet<Tag> Tags => Set<Tag>();
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+		builder.ApplyConfigurationsFromAssembly(typeof(AppContext).Assembly);
+    }
+}
---
+++ TechHub.Infrastructure/Data/ApplicationUser.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using TechHub.Core.Entities;
+
+namespace TechHub.Infrastructure.Data;
+
+#nullable disable
+
+internal class ApplicationUser : IdentityUser, IApplicationUser
+{
+    public string DisplayName { get; set; }
+    public string AvatarPath { get; set; }
+    public DateTime DateCreated { get; set; }
+    public DateTime DateModified { get; set; }
+
+    public ICollection<Post> Posts { get; set; }
+}
