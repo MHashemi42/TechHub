84f14e1065f5ebbfa62d2bdfe8c6356589e7cf27
Mohammad Hashemi <mohammadhashemi42@gmail.com>
2023-03-13 22:31:08 +0330

    override SaveChangesAsync method to init IEntityBase properties

--- TechHub.Infrastructure/Data/AppDbContext.cs
+++ TechHub.Infrastructure/Data/AppDbContext.cs
@@ -1,22 +1,46 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 using TechHub.Core.Entities;
 
 namespace TechHub.Infrastructure.Data;
 
 internal class AppDbContext : IdentityDbContext<ApplicationUser>
 {
-	public AppDbContext(DbContextOptions<AppDbContext> options)
-		: base(options)
-	{
-	}
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
 
-	public DbSet<Post> Posts => Set<Post>();
-	public DbSet<Tag> Tags => Set<Tag>();
+    public DbSet<Post> Posts => Set<Post>();
+    public DbSet<Tag> Tags => Set<Tag>();
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
-		builder.ApplyConfigurationsFromAssembly(typeof(AppContext).Assembly);
+        builder.ApplyConfigurationsFromAssembly(typeof(AppContext).Assembly);
+    }
+
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        IEnumerable<EntityEntry> entries = ChangeTracker
+                        .Entries()
+                        .Where(e => e.Entity is IEntityBase &&
+                                (e.State == EntityState.Added ||
+                                e.State == EntityState.Modified));
+
+        foreach (EntityEntry entityEntry in entries)
+        {
+            DateTime utcNow = DateTime.UtcNow;
+
+            ((IEntityBase)entityEntry.Entity).DateModified = utcNow;
+
+            if (entityEntry.State == EntityState.Added)
+            {
+                ((IEntityBase)entityEntry.Entity).DateCreated = utcNow;
+            }
+        }
+
+        return base.SaveChangesAsync(cancellationToken);
     }
 }
