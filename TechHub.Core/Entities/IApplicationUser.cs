b7b2dbb28ea82914fffa88d5c2f556634ee6e78a
Mohammad Hashemi <mohammadhashemi42@gmail.com>
2023-03-13 20:26:59 +0330

    Change AvatarPath property of IApplicationUser entity to be nullable

--- TechHub.Core/Entities/IApplicationUser.cs
+++ TechHub.Core/Entities/IApplicationUser.cs
@@ -5,5 +5,5 @@ public interface IApplicationUser : IEntityBase<string>
     public string UserName { get; set; }
     public string DisplayName { get; set; }
     public string Email { get; set; }
-    public string AvatarPath { get; set; }
+    public string? AvatarPath { get; set; }
 }
