6878e62d45160e3be0fc5cbbac0237fe12f54e5b
Mohammad Hashemi <mohammadhashemi42@gmail.com>
2023-03-13 22:34:37 +0330

    Add configuration for post and tag entities

---
+++ TechHub.Infrastructure/Data/Configurations/PostConfiguration.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TechHub.Core.Entities;
+
+namespace TechHub.Infrastructure.Data.Configurations;
+
+internal class PostConfiguration : IEntityTypeConfiguration<Post>
+{
+    public void Configure(EntityTypeBuilder<Post> post)
+    {
+        post.Property(p => p.Title)
+            .IsRequired()
+            .HasMaxLength(150);
+
+        post.Property(p => p.Slug)
+            .IsRequired()
+            .HasMaxLength(150);
+
+        post.Property(p => p.Content)
+            .IsRequired();
+
+        post.Property(p => p.ThumbnailPath)
+            .IsRequired()
+            .HasMaxLength(500);
+
+        post.HasOne(p => p.Author)
+            .WithMany(a => a.Posts);
+
+        post.HasMany(p => p.Tags)
+            .WithMany(t => t.Posts);
+    }
+}
---
+++ TechHub.Infrastructure/Data/Configurations/TagConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TechHub.Core.Entities;
+
+namespace TechHub.Infrastructure.Data.Configurations;
+
+internal class TagConfiguration : IEntityTypeConfiguration<Tag>
+{
+    public void Configure(EntityTypeBuilder<Tag> tag)
+    {
+        tag.Property(t => t.Name)
+           .IsRequired()
+           .HasMaxLength(150);
+
+        tag.Property(t => t.Slug)
+           .IsRequired()
+           .HasMaxLength(200);
+
+        tag.HasMany(t => t.Posts)
+           .WithMany(p => p.Tags);
+    }
+}
