8637d2bd27162be7f35b2b9800c554b5034684ae
Mohammad Hashemi <mohammadhashemi42@gmail.com>
2023-03-14 13:06:59 +0330

    Add ApplicationUserConfiguration

---
+++ TechHub.Infrastructure/Data/Configurations/ApplicationUserConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace TechHub.Infrastructure.Data.Configurations;
+
+internal class ApplicationUserConfiguration : IEntityTypeConfiguration<ApplicationUser>
+{
+    public void Configure(EntityTypeBuilder<ApplicationUser> user)
+    {
+        user.HasMany(u => u.Posts)
+            .WithOne();
+    }
+}
