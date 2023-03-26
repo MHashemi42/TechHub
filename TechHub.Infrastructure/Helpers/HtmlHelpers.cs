0bcbec1e6cc1b2fa36299330f7955f9b56f3d150
Mohammad Hashemi <mohammadhashemi42@gmail.com>
2023-03-26 17:33:35 +0330

    Add HtmlHelpers for getting the first paragraph from a html

---
+++ TechHub.Infrastructure/Helpers/HtmlHelpers.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.RegularExpressions;
+
+namespace TechHub.Infrastructure.Helpers;
+
+internal static class HtmlHelpers
+{
+    public static string GetFirstParagraph(string html)
+    {
+        Match match = RegexHelpers.GetFirstParagraphFromHtml().Match(html);
+        if (match.Success)
+        {
+            return match.Groups[1].Value;
+        }
+
+        return string.Empty;
+    }
+}
---
+++ TechHub.Infrastructure/Helpers/RegexHelpers.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.RegularExpressions;
+
+namespace TechHub.Infrastructure.Helpers;
+
+internal static partial class RegexHelpers
+{
+    [GeneratedRegex("<p>\\s*(.+?)\\s*</p>")]
+    public static partial Regex GetFirstParagraphFromHtml();
+}
