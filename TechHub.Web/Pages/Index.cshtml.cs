ede44669a8b3b7be4e4b32b791a925ef1e94e3e3
Mohammad Hashemi <mohammadhashemi42@gmail.com>
2023-03-26 19:58:03 +0330

    Update Index page to show PostSummaryDtos

--- TechHub.Web/Pages/Index.cshtml.cs
+++ TechHub.Web/Pages/Index.cshtml.cs
@@ -1,11 +1,28 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using TechHub.Core.DTOs;
+using TechHub.Core.Services;
 
 namespace TechHub.Web.Pages;
 
 public class IndexModel : PageModel
 {
-    public void OnGet()
+    private readonly IPostService _postService;
+    public IEnumerable<PostSummaryDto> PostSummaryDtos { get; set; }
+        = Enumerable.Empty<PostSummaryDto>();
+    public IndexModel(IPostService postService)
     {
+        _postService = postService;
+    }
+
+    public async Task<IActionResult> OnGetAsync(CancellationToken cancellationToken)
+    {
+        PostSummaryDtos = await _postService.GetPostSummaryDtosAsync(cancellationToken);
+        if (!PostSummaryDtos.Any())
+        {
+            return RedirectToPage("NotFound");
+        }
+
+        return Page();
     }
 }
