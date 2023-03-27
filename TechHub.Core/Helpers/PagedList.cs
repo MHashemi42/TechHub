ade225b05c1f80f0607cfe53389162f8946104b0
Mohammad Hashemi <mohammadhashemi42@gmail.com>
2023-03-27 15:40:04 +0330

    Add PagedList for pagination

---
+++ TechHub.Core/Helpers/PagedList.cs
@@ -0,0 +1,29 @@
+﻿namespace TechHub.Core.Helpers;
+
+public class PagedList<T> : List<T>
+{
+    public PagedListMetaData MetaData { get; init; }
+
+    public PagedList()
+    {
+        MetaData = new PagedListMetaData();
+    }
+
+    public PagedList(IEnumerable<T> items, int count, int pageNumber, int pageSize)
+    {
+        MetaData = new PagedListMetaData
+        {
+            TotalCount = count,
+            PageSize = pageSize,
+            CurrentPage = pageNumber
+        };
+
+        AddRange(items);
+    }
+
+    public PagedList(IEnumerable<T> items, PagedListMetaData metaData)
+    {
+        MetaData = metaData;
+        AddRange(items);
+    }
+}
