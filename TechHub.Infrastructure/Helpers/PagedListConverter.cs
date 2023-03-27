cf564e86ca7c44ea943d46ff30469573c87c8ca3
Mohammad Hashemi <mohammadhashemi42@gmail.com>
2023-03-27 15:43:01 +0330

    Add PagedListConverter to map PagedList with automapper

---
+++ TechHub.Infrastructure/Helpers/PagedListConverter.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using TechHub.Core.Helpers;
+
+namespace TechHub.Infrastructure.Helpers;
+
+public class PagedListConverter<TIn, TOut> : ITypeConverter<PagedList<TIn>, PagedList<TOut>>
+{
+    public PagedList<TOut> Convert(PagedList<TIn> source, PagedList<TOut> destination,
+        ResolutionContext context)
+    {
+        IEnumerable<TOut> items = context.Mapper.Map<IEnumerable<TOut>>(source);
+
+        return new PagedList<TOut>(items, source.MetaData);
+    }
+}
--- TechHub.Infrastructure/Profiles/PostProfile.cs
+++ TechHub.Infrastructure/Profiles/PostProfile.cs
@@ -2,24 +2,28 @@
 using DNTPersianUtils.Core;
 using TechHub.Core.DTOs;
 using TechHub.Core.Entities;
+using TechHub.Core.Helpers;
 using TechHub.Infrastructure.Helpers;
 
 namespace TechHub.Infrastructure.Profiles;
 
 internal class PostProfile : Profile
 {
-	public PostProfile()
-	{
-		bool appendHhMm = true;
-		bool convertToIranTimeZone = true;
-		bool includePersianDate = true;
+    public PostProfile()
+    {
+        bool appendHhMm = true;
+        bool convertToIranTimeZone = true;
+        bool includePersianDate = true;
 
-		CreateMap<Post, PostSummaryDto>()
-			.ForMember(p => p.PreviewContent,
-						m => m.MapFrom(p => HtmlHelpers.GetFirstParagraph(p.Content)))
-			.ForMember(p => p.DatePublished,
-						m => m.MapFrom(p => p.DatePublished
-						.ToFriendlyPersianDateTextify(appendHhMm, convertToIranTimeZone,
-													  includePersianDate)));
-	}
+        CreateMap<Post, PostSummaryDto>()
+            .ForMember(p => p.PreviewContent,
+                        m => m.MapFrom(p => HtmlHelpers.GetFirstParagraph(p.Content)))
+            .ForMember(p => p.DatePublished,
+                        m => m.MapFrom(p => p.DatePublished
+                        .ToFriendlyPersianDateTextify(appendHhMm, convertToIranTimeZone,
+                                                      includePersianDate)));
+
+        CreateMap<PagedList<Post>, PagedList<PostSummaryDto>>()
+            .ConvertUsing<PagedListConverter<Post, PostSummaryDto>>();
+    }
 }
