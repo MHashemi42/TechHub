b09d0a0ecdc2f295ee836fbe24956c5f4ad90405
Mohammad Hashemi <mohammadhashemi42@gmail.com>
2023-03-28 16:15:43 +0330

    Add unit test for GetFirstParagraphFromHtml method of HtmlHelpers class

---
+++ TechHub.Infrastructure.Tests/Helpers/HtmlHelpersTests.cs
@@ -0,0 +1,46 @@
+﻿namespace TechHub.Infrastructure.Tests.Helpers;
+
+public class HtmlHelpersTests
+{
+    [Theory]
+    [ClassData(typeof(GetFirstParagraphFromHtmlTestData))]
+    void GetFirstParagraphFromHtmlShouldReturnExpectedStringTheory(string expected, string html)
+    {
+        string firstParagraph = HtmlHelpers.GetFirstParagraphFromHtml(html);
+        Assert.Equal(expected, firstParagraph);
+    }
+
+    public class GetFirstParagraphFromHtmlTestData : TheoryData<string, string>
+    {
+        public GetFirstParagraphFromHtmlTestData()
+        {
+            string expected = string.Empty;
+            string html = string.Empty;
+            Add(expected, html);
+
+            expected = "Just a paragraph";
+            html = $"<p>{expected}</p>";
+            Add(expected, html);
+
+            expected = string.Empty;
+            html = "only text, not html";
+            Add(expected, html);
+
+            expected = "first paragraph";
+            html = $"<p>{expected}</p><p>second paragraph</p>";
+            Add(expected, html);
+
+            expected = "a paragraph inside of a div element";
+            html = $"<div><p>{expected}</p></div><p>another paragraph</p>";
+            Add(expected, html);
+
+            expected = "a paragraph with <i>italic</i> element";
+            html = $"<p>{expected}</p>";
+            Add(expected, html);
+
+            expected = "a paragraph with class attribute";
+            html = $"<p class=\"h4\">{expected}</p>";
+            Add(expected, html);
+        }
+    }
+}
