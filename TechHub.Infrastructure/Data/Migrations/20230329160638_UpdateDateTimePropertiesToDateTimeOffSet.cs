eb681f255717141a75a5b9ef67b9a55fd303932b
Mohammad Hashemi <mohammadhashemi42@gmail.com>
2023-03-31 12:39:10 +0330

    Add migration

---
+++ TechHub.Infrastructure/Data/Migrations/20230329160638_UpdateDateTimePropertiesToDateTimeOffSet.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TechHub.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateDateTimePropertiesToDateTimeOffSet : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedOnUtc",
+                table: "Tags");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedOnUtc",
+                table: "Tags");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedOnUtc",
+                table: "Posts");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedOnUtc",
+                table: "Posts");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedOnUtc",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedOnUtc",
+                table: "AspNetUsers");
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "CreatedOn",
+                table: "Tags",
+                type: "datetimeoffset",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "ModifiedOn",
+                table: "Tags",
+                type: "datetimeoffset",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "DatePublished",
+                table: "Posts",
+                type: "datetimeoffset",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "CreatedOn",
+                table: "Posts",
+                type: "datetimeoffset",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "ModifiedOn",
+                table: "Posts",
+                type: "datetimeoffset",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "CreatedOn",
+                table: "AspNetUsers",
+                type: "datetimeoffset",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "ModifiedOn",
+                table: "AspNetUsers",
+                type: "datetimeoffset",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedOn",
+                table: "Tags");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedOn",
+                table: "Tags");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedOn",
+                table: "Posts");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedOn",
+                table: "Posts");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedOn",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedOn",
+                table: "AspNetUsers");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedOnUtc",
+                table: "Tags",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedOnUtc",
+                table: "Tags",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DatePublished",
+                table: "Posts",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "datetimeoffset");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedOnUtc",
+                table: "Posts",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedOnUtc",
+                table: "Posts",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedOnUtc",
+                table: "AspNetUsers",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedOnUtc",
+                table: "AspNetUsers",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+    }
+}
