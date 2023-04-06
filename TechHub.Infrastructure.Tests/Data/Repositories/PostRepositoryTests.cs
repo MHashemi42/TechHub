268c51fb1d4338802d525c01851b2fbdaa768322
Mohammad Hashemi <mohammadhashemi42@gmail.com>
2023-04-06 15:40:40 +0330

    Refactor PostRepositoryTests

--- TechHub.Infrastructure.Tests/Data/Repositories/PostRepositoryTests.cs
+++ TechHub.Infrastructure.Tests/Data/Repositories/PostRepositoryTests.cs
@@ -8,70 +8,93 @@ namespace TechHub.Infrastructure.Tests.Data.Repositories;
 
 public class PostRepositoryTests
 {
-    private readonly PostRepository _sut;
-    private readonly UnitOfWork _unitOfWork;
-    public PostRepositoryTests()
+    [Theory]
+    [InlineData(-1)]
+    [InlineData(0)]
+    public async Task GetAllAsync_NegativeOrZeroCurrentPage_ThrowsArgumentException(int currentPage)
+    {
+        AppDbContext dbContext = CreateInMemoryDbContext();
+        PostRepository sut = CreateDefaultPostRepository(dbContext);
+
+        await Assert.ThrowsAsync<ArgumentException>(() => 
+            sut.GetAllAsync(currentPage, pageSize: 1));
+    }
+
+    [Theory]
+    [InlineData(-1)]
+    [InlineData(0)]
+    public async Task GetAllAsync_NegativeOrZeroPageSize_ThrowsArgumentException(int pageSize)
+    {
+        AppDbContext dbContext = CreateInMemoryDbContext();
+        PostRepository sut = CreateDefaultPostRepository(dbContext);
+
+        await Assert.ThrowsAsync<ArgumentException>(() => 
+            sut.GetAllAsync(currentPage: 1, pageSize));
+    }
+
+    [Theory]
+    [InlineData(-1)]
+    [InlineData(0)]
+    public async Task GetByIdAsync_NegativeOrZeroId_ThrowsArgumentException(int id)
+    {
+        AppDbContext dbContext = CreateInMemoryDbContext();
+        PostRepository sut = CreateDefaultPostRepository(dbContext);
+
+        await Assert.ThrowsAsync<ArgumentException>(() => sut.GetByIdAsync(id));
+    }
+
+    [Theory]
+    [InlineData(2, 3, 3)]
+    [InlineData(2, 6, 4)]
+    [InlineData(4, 5, 0)]
+    public async Task GetAllAsync_Pagination_ReturnsExpectedCount(int currentPage,
+        int pageSize, int expectedCount)
+    {
+        AppDbContext dbContext = CreateInMemoryDbContext();
+        PostRepository sut = CreateDefaultPostRepository(dbContext);
+        UnitOfWork unitOfWork = CreateDefaultUnitOfWork(dbContext);
+        const int TOTAL_POST_COUNT = 10;
+        for (int i = 0; i < TOTAL_POST_COUNT; i++)
+        {
+            Post post = CreateTempPost();
+            sut.Add(post);
+        }
+        await unitOfWork.SaveChangesAsync();
+
+        PagedList<Post> posts = await sut.GetAllAsync(currentPage, pageSize);
+        int actualCount = posts.Count;
+
+        Assert.Equal(expectedCount, actualCount);
+    }
+
+    private static Post CreateTempPost()
+    {
+        return new Post
+        {
+            DatePublished = DateTimeOffset.UtcNow,
+            AuthorId = nameof(Post.AuthorId),
+            Content = nameof(Post.Content),
+            Slug = nameof(Post.Slug),
+            ThumbnailPath = nameof(Post.ThumbnailPath),
+            Title = nameof(Post.Title)
+        };
+    }
+
+    private static AppDbContext CreateInMemoryDbContext()
     {
         var dbContextOptionsBuilder = new DbContextOptionsBuilder<AppDbContext>()
             .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString());
-        var dbContext = new AppDbContext(dbContextOptionsBuilder.Options);
 
-        _sut = new PostRepository(dbContext);
-        _unitOfWork = new UnitOfWork(dbContext);
+        return new AppDbContext(dbContextOptionsBuilder.Options);
     }
 
-    [Fact]
-    public async Task GetAllAsync_ShouldThrowArgumentExceptionForNegativeOrZeroCurrentPage()
+    private static PostRepository CreateDefaultPostRepository(AppDbContext dbContext)
     {
-        await Assert.ThrowsAsync<ArgumentException>(() =>
-            _sut.GetAllAsync(currentPage: -1, pageSize: 1));
-        await Assert.ThrowsAsync<ArgumentException>(() =>
-            _sut.GetAllAsync(currentPage: 0, pageSize: 1));
+        return new PostRepository(dbContext);
     }
 
-    [Fact]
-    public async Task GetAllAsync_ShouldThrowArgumentExceptionForNegativeOrZeroPageSize()
+    private static UnitOfWork CreateDefaultUnitOfWork(AppDbContext dbContext)
     {
-        await Assert.ThrowsAsync<ArgumentException>(() => 
-            _sut.GetAllAsync(currentPage: 1, pageSize: -1));
-        await Assert.ThrowsAsync<ArgumentException>(() =>
-            _sut.GetAllAsync(currentPage: 1, pageSize: 0));
-    }
-
-    [Fact]
-    public async Task GetByIdAsync_ShouldThrowArgumentExceptionForNegativeOrZeroId()
-    {
-        await Assert.ThrowsAsync<ArgumentException>(() => _sut.GetByIdAsync(id: -1));
-        await Assert.ThrowsAsync<ArgumentException>(() => _sut.GetByIdAsync(id: 0));
-    }
-
-    [Fact]
-    public async Task GetAllAsync_ShouldReturnExpectedPagedListCount()
-    {
-        int totalPostCount = 10;
-        DateTimeOffset utcNow = DateTimeOffset.UtcNow;
-        for (int i = 0; i < totalPostCount; i++)
-        {
-            _sut.Add(new Post
-            {
-                DatePublished = utcNow,
-                AuthorId = "authorId",
-                Content = "content",
-                Slug = "slug",
-                ThumbnailPath = "thumbnailPath",
-                Title = "title"
-            });
-        }
-
-        await _unitOfWork.SaveChangesAsync();
-
-        PagedList<Post> posts = await _sut.GetAllAsync(currentPage: 2, pageSize: 3);
-        Assert.Equal(expected: 3, actual: posts.Count);
-
-        posts = await _sut.GetAllAsync(currentPage: 2, pageSize: 6);
-        Assert.Equal(expected: 4, actual: posts.Count);
-
-        posts = await _sut.GetAllAsync(currentPage: 4, pageSize: 5);
-        Assert.Empty(posts);
+        return new UnitOfWork(dbContext);
     }
 }
